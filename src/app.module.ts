6f7232f959c612766195f4ee8dcdef84633f3b3b
Omar Alsalahy <omar.alsalahy@roit.com.br>
2021-03-03 19:56:06 -0300

    Remove employee module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from './user/user.module';
-import { EmployeeModule } from './employee/employee.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(), 
-    UserModule, EmployeeModule
+    UserModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
