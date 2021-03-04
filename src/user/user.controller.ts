380bf94ef865f56f6ba4990a793ef902f659c96a
Omar Alsalahy <omar.alsalahy@roit.com.br>
2021-03-03 23:30:02 -0300

    Use github and viacep api's

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,21 +2,43 @@ import { Controller, Get, Post, Delete , Put, Body, Param
 } from '@nestjs/common';
 import  UserDto        from "./dto/create-user.dto";
 import  User           from "./interface/user.interface";
+import  Address        from "./interface/address.interface";
 import { UserService } from "./user.service";
+import axios           from "axios";
 
 @Controller('user')
 export class UserController {
 
   constructor(private userService : UserService){}
 
-  @Get("github/:username")
-  getGithub(@Param("username") username : string): string{
-    return "Github information";
+  /*
+   * Receives a github user account and searches the github api 
+   * @param github
+   * @return github response
+  */
+  @Get(":id/github")
+  async getGithub(@Param("id") id : number): Promise<string>{
+    var githubUrl = "https://api.github.com/search/users?q=";
+    /* Confirm the user object already exists in the UserService */
+    var user : User = this.userService.get(id);
+
+    return await axios.get(githubUrl + user.githubAccount);
   }
 
-  @Get("cep/:address")
-  getCep(@Param("address") address : string): string{
-    return "Cep information";
+  @Get(":id/cep")
+  async getCep(@Param("id") id : number): Promise<String>{
+    var user : User = this.userService.get(id);
+    var address : Address = user.address;
+
+    if(user != null){
+      var viacepUrl = "https://viacep.com.br/ws"
+      var url = `${viacepUrl}/${address.state}/${address.city}/
+        ${address.street}/json`;
+
+      return await axios.get(url);
+    }else{
+      return `Nothing found for ${id}`;
+    }
   }
 
   @Post()
